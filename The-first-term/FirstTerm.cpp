f21c9869034b3b7225368760d53e6a1021e39084
0Dmitriy0 <dd.petrashchak@gmail.com>
2020-06-08 22:31:05 +0300

    The-first-term

---
+++ The-first-term/FirstTerm.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, const char* argv[])
+{
+    string name, surnames;
+    cout << "Enter your name: ";
+    getline (cin, name);
+    cout << "Enter your surnames: ";
+    getline (cin, surnames);
+    
+    cout << name + surnames;
+
+    cin.get();
+    return 0;
+}
---
+++ The-first-term/RandNumber.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char* argv[]){
+
+    cout << "1-random number -> " << 1 + rand() % 80 << endl;
+    cout << "2-random number -> " << 1 + rand() % 80 << endl;
+    cout << "3-random number -> " << 1 + rand() % 80 << endl;
+    cout << "4-random number -> " << 1 + rand() % 80 << endl;
+    cout << "5-random number -> " << 1 + rand() % 80 << endl;
+    cout << "6-random number -> " << 1 + rand() % 80 << endl;
+    cout << "7-random number -> " << 1 + rand() % 80 << endl;
+    cout << "8-random number -> " << 1 + rand() % 80 << endl;
+
+    cin.get();
+    return 0;
+}
