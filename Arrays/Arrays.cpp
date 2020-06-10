8a55b5e0c88958faad1cebdd07ca1b815a39b56f
0Dmitriy0 <dd.petrashchak@gmail.com>
2020-06-10 21:08:25 +0300

    Create Arrays.cpp

---
+++ Arrays/Arrays.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int arr[] {3, 7, 5 };
+	//! cout << arr[0];
+	for (int i = 0; i < 3; i++)
+	{
+		cout << " " << arr[i];
+	}
+
+	system("pause");
+	return 0;
+}
