9551e3e9cee454eebdce92fb412aee682ebef0b5
0Dmitriy0 <dd.petrashchak@gmail.com>
2020-06-10 08:46:07 +0300

    Update HelloWorld.cpp

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -4,41 +4,40 @@
 using namespace std;
 
 int main(){
-    string name, like;
-    int Good, Badly;
-    Good = 1;
-    Badly = 0;
+	 string name, like;
+	 int Good, Badly;
+	 Good = true;
+	 Badly = false;
 
-    cout << "Hello World!" << endl;
-    cout << "What is your name? -> ";
-    getline(cin, name);
-    cout << "Hello " << name << endl;
-    cout << "_______________\
-             |1:Good or 2:Badly|\
-             ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾" << endl;
-    cout << "Like you? -> ";
-    getline(cin, like);
+	 cout << "Hello World!" << endl;
+	 cout << "What is your name? -> ";
+	 getline(cin, name);
+	 cout << "Hello " << name << endl;
+	 cout << "|Good or Badly|" << endl;
+	 cout << "|true    false|" << endl;
+	 cout << "Like you? -> ";
+	 getline(cin, like);
+	/*
+	 if (int _Good = 1, _Good > Badly, _Good++){
+		  Good = _Good;
+		  cout << "It's good" << endl;
+		  cout << "But I need to go" << endl;
+		  cout << "Good bye " << name << endl;
+		  cin >> Good;
 
-    if (int _Good = 1, _Good > Badly, _Good++){
-        Good = _Good;
-        cout << "It's good" << endl;
-        cout << "But I need to go" << endl;
-        cout << "Good bye " << name << endl;
-        cin >> Good;
+		  return 0;
+	 }
+	 if (int _Badly = 0, _Badly < Good, _Badly++){
+		  Badly = _Badly;
+		  cout << "This is bad" << endl;
+		  cout << "I'm sorry but I need to leave" << endl;
+		  cout << "You just do not be sad" << endl;
+		  cout << "Good bye " << name << endl;
+		  cin >> Badly;
 
-        return 0;
-    }
-    if (int _Badly = 0, _Badly < Good, _Badly++){
-        Badly = _Badly;
-        cout << "This is bad" << endl;
-        cout << "I'm sorry but I need to leave" << endl;
-        cout << "You just do not be sad" << endl;
-        cout << "Good bye " << name << endl;
-        cin >> Badly;
-
-        return 0;
-    }
-
-    cin.get();
-    return 0;
+		  return 0;
+	 }
+	*/
+	 cin.get();
+	 return 0;
 }
