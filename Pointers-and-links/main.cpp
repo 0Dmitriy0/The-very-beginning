5d51ea332f39815b9bfe998d9103359b9a6bd384
0Dmitriy0 <dd.petrashchak@gmail.com>
2020-06-11 23:15:30 +0300

    Create main.cpp

---
+++ Pointers-and-links/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+	int value = 8;
+	int *ptrValue = &value;
+
+	cin >> *ptrValue;
+	cout << ptrValue << endl;
+	cout << endl;
+
+	cout << &value << " - " << ptrValue << endl;
+
+	return 0;
+}
