057056578da0bc482da66aba271ca0f8d335ff4d
0Dmitriy0 <dd.petrashchak@gmail.com>
2020-06-14 00:04:30 +0300

    Create Change.cpp

---
+++ Data-types/Change.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	//! Тип_Данных | Имя_Переменой | Значения_Переменой
+	bool t = true, y = false;
+	char g = 'r';
+	double d = 1.1551;
+	int Age = 18, x, f;
+	int a = 5;
+	cout << a << endl << endl;
+
+	a = 10;
+	cout << a << endl << endl;
+
+	x = 19;
+	f = 14;
+
+	cout << Age << " " << x << " " << f << endl << endl;
+	cout << d << endl << endl;
+	cout << g << endl << endl;
+
+	cout << "True = 1 or False = 0" << endl;
+	cout << t << "\t\t    " << y << endl;
+
+	return 0;
+}
