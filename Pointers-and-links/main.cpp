ab0f83ba9edf67792f65b7cdd11e5707c162e620
0Dmitriy0 <dd.petrashchak@gmail.com>
2020-06-14 00:04:44 +0300

    Update main.cpp

--- Pointers-and-links/main.cpp
+++ Pointers-and-links/main.cpp
@@ -8,10 +8,7 @@ int main(int argc, char const *argv[]){
 	int value = 8;
 	int *ptrValue = &value;
 
-	cin >> *ptrValue;
-	cout << ptrValue << endl;
-	cout << endl;
-
+	//cin >> *ptrValue;
 	cout << &value << " - " << ptrValue << endl;
 
 	return 0;
