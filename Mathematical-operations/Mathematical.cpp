b31d83f00a720deedf6a1b1f3cd0e3352877fa29
0Dmitriy0 <dd.petrashchak@gmail.com>
2020-06-13 18:33:09 +0300

    Create Mathematical.cpp

---
+++ Mathematical-operations/Mathematical.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	cout << "Pow -> " << pow(2, 2) << endl;//! The first number is common and the second degree.
+	cout << "Abs -> " << abs(-23) << endl;
+	cout << "Sin -> " << sin(10) << endl;
+	cout << "Cos -> " << cos(10) << endl;
+	cout << "Sqrt -> " << sqrt(16) << endl;
+	cout << "Ceil -> " << ceil(23.67) << endl;
+	cout << "Floor -> " << floor(23.67) << endl;
+	cout << "Exp -> " << exp(2) << endl;
+	cout << "Log -> " << log(4) << endl;
+	cout << "Log10 -> " << log10(4) << endl;
+
+	return 0;
+}
